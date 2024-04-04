71e78afe33a10bc1d183b75805494581c856dec0
codingaring <sksms2104@naver.com>
2024-04-04 13:38:10 +0900

    ✨ Add Link

--- components/home/LandingHeader/LandingHeader.tsx
+++ components/home/LandingHeader/LandingHeader.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { HERO_IMAGE_SRC } from "../constant";
 import * as S from "./LandingHeaderStyled";
 import Image from "next/image";
@@ -9,7 +10,9 @@ export default function LandingHeader() {
         <S.HightLightText>세상의 모든 정보</S.HightLightText>를
         <S.WordBreak /> 쉽게 저장하고 관리해 보세요
       </S.HeroTitle>
-      <S.LandingButton>링크 추가하기</S.LandingButton>
+      <Link href="/signup">
+        <S.LandingButton>링크 추가하기</S.LandingButton>
+      </Link>
       <S.HeroImageBox>
         <Image fill src={HERO_IMAGE_SRC} alt="링크브러리 메인 화면" />
       </S.HeroImageBox>
