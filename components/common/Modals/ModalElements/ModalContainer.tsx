a32d143862ee74e57f905de40d10d35adb84e9bf
codingaring <sksms2104@naver.com>
2024-04-08 21:38:14 +0900

    🐛 Fix : resolve problem _ unfixed center modal

--- components/common/Modals/ModalElements/ModalContainer.tsx
+++ components/common/Modals/ModalElements/ModalContainer.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 import styled from "styled-components";
 
 export const ContainerModal = styled.div`
-  position: absolute;
+  position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
