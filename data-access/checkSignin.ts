64c61be6ff23e10e08302fed3c36788e66ebf474
codingaring <sksms2104@naver.com>
2024-05-12 13:58:35 +0900

    ♻️ Refactor : modify change api url : checkSignin

--- data-access/checkSignin.ts
+++ data-access/checkSignin.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
+import { axiosInstance } from "./axios/axiosInstance";
 
 export async function checkSignin(trySignValue: {
   email: string;
   password: string;
 }) {
-  const response = await axios.post(`/linkbrary/v1/auth/sign-in`, {
+  const response = await axiosInstance.post(`/linkbrary/v1/auth/sign-in`, {
     email: trySignValue.email,
     password: trySignValue.password,
   });
