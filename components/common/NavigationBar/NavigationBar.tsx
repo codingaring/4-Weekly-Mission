6924ca32688988510378e2b91bfe0e2fcaaaa88d
codingaring <sksms2104@naver.com>
2024-05-17 10:32:58 +0900

    🐛 Fix : resolve conflict

--- components/common/NavigationBar/NavigationBar.tsx
+++ components/common/NavigationBar/NavigationBar.tsx
@@ -4,22 +4,12 @@ import { LOGO_IMAGE, TEXT } from "./constant";
 import { useRouter } from "next/router";
 import { ROUTE } from "@util/constant";
 import { getLoginUserInfo } from "@data-access/getLoginUserInfo";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { UserContext } from "context/UserContext";
 import { useQuery } from "@tanstack/react-query";
 
-interface ProfileState {
-  auth_id: string;
-  email: string;
-  image_source: string;
-}
-
 export const NavigationBar = () => {
   const { handleUserDataState } = useContext(UserContext);
-<<<<<<< HEAD
-  const [profile, setProfile] = useState<ProfileState>();
-=======
->>>>>>> a23006954a5f04461317edcce92949be866d067c
   const Location = useRouter();
   const LocationPath = Location.pathname;
   const { data: profile } = useQuery({
@@ -27,26 +17,11 @@ export const NavigationBar = () => {
     queryFn: getLoginUserInfo,
   });
 
-<<<<<<< HEAD
-  async function handleLoadUserInfo() {
-    try {
-      const { data } = await getLoginUserInfo();
-      const { email, image_source, auth_id } = data[0] || [];
-      setProfile({
-        auth_id: auth_id,
-        email: email,
-        image_source: image_source,
-      });
-      handleUserDataState({ userId: auth_id });
-    } catch (error) {
-      console.error(Error);
-=======
   useEffect(() => {
     if (profile) {
       handleUserDataState({ isLogin: true, userId: profile.id });
->>>>>>> a23006954a5f04461317edcce92949be866d067c
     }
-  }, [handleUserDataState, profile]);
+  }, [profile]);
 
   return (
     <S.NavigationBarContainer pathName={LocationPath}>
