711d90f208ca9c5cd8bfdd5c949a82f7bdc57394
codingaring <sksms2104@naver.com>
2024-05-28 12:29:06 +0900

    ♻️ Refactor : handleToken window === undefined , throw error

--- util/handleToken.ts
+++ util/handleToken.ts
@@ -5,6 +5,8 @@ export const setToken = (accessToken: string, refreshToken: string) => {
   if (typeof window !== "undefined") {
     localStorage.setItem(ACCESS_TOKEN, accessToken);
     localStorage.setItem(REFRESH_TOKEN, refreshToken);
+  } else {
+    throw new Error("토큰을 저장하지 못했습니다.");
   }
 };
 
@@ -14,6 +16,8 @@ export const getToken = () => {
     const refreshToken = localStorage.getItem(REFRESH_TOKEN);
 
     return { accessToken, refreshToken };
+  }else {
+    throw new Error("토큰을 불러오지 못했습니다.");
   }
 };
 
@@ -21,5 +25,7 @@ export const removeToken = () => {
   if (typeof window !== "undefined") {
     localStorage.removeItem(ACCESS_TOKEN);
     localStorage.removeItem(REFRESH_TOKEN);
+  }else {
+    throw new Error("토큰 삭제하지 못했습니다.");
   }
 };
